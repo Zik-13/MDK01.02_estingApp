3e3faf0a63accd613919869a9793b09d6aeee114
zikcruel@mail.ru <zikcruel@mail.ru>
2022-11-18 14:28:49 +0300

    Добавьте файлы проекта.

---
+++ WhiteBox/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfApp1;
+using System;
+
+namespace WhiteBox
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Ravnostoronyq_5_6_7_Raznostoronniy()
+        {
+            int x = 5;
+            int y = 6;
+            int z = 7;
+            string result ="Разносторнний";
+            
+            Treygolnik treygolnik = new Treygolnik();
+            Assert.AreEqual(result, treygolnik.Info(x, y, z));          
+        }
+
+        [TestMethod]
+        public void Ravnostoronyq_6_6_7_Ravnoberyniq()
+        {
+            int x = 6;
+            int y = 6;
+            int z = 7;
+            string result = "Равнобедренный";
+
+            Treygolnik treygolnik = new Treygolnik();
+            Assert.AreEqual(result, treygolnik.Info(x, y, z));
+        }
+
+        [TestMethod]
+        public void Ravnostoronyq_6_6_6_Ravnostoronyq()
+        {
+            int x = 6;
+            int y = 6;
+            int z = 6;
+            string result = "Равностороний";
+
+            Treygolnik treygolnik = new Treygolnik();
+            Assert.AreEqual(result, treygolnik.Info(x, y, z));
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            try
+            {
+                num1 = Convert.ToInt32(first.Text);
+                num2 = Convert.ToInt32(second.Text);
+                num3 = Convert.ToInt32(frist.Text);
+            }
+            catch (FormatException) { MessageBox.Show("Вы ввели не правильные данные"); }
+
+
+            Treygolnik treygolnik = new Treygolnik();
+            result.Text = treygolnik.Info(num1, num2, num3);
+            if (treygolnik.Info(num1, num2, num3) == "ERROR")
+            {
+                MessageBox.Show("Вы ввели не правильные стороны треугольника");
+            }
+
+            
+
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+
+        }
+    }
+}
